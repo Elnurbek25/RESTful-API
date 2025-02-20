eb10323665ba3fc344e090d48399a695ca6bc663
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-20 19:41:38 +0500

    Created api.php for defining API routes

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('posts', PostController::class);
+Route::apiResource('users', UserController::class);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user}', [UserController::class, 'show']);
