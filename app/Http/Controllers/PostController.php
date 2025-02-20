027359ec17fd52b43b5987b98d22f6839e111552
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-20 19:30:27 +0500

    Created PostController with methods for creating posts and added routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,36 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+
 
 class PostController extends Controller
 {
-    
+
     public function index()
     {
-        
+        $posts = Post::with('user')->get(); 
+        return response()->json([
+            'data' => $posts,
+        ],200);
     }
-
-   
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        
-    }
-
+        $post = Post::create($request->validated());
     
+        return response()->json([
+            'data' => $post,
+        ], 201);
+    }
     public function show(string $id)
     {
-        
+        $post = Post::findOrFail($id);
+        return response()->json([$post]);
     }
-
-    
-    public function update(Request $request, string $id)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        
+        $post->update($request->validated());
+    
+        return response()->json([
+            'data' => $post,
+        ],200);
     }
-
-    
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        
+        $post->delete();   
+        return response()->json([
+        ],200);
     }
 }
